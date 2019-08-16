ad9889950ca0f905b018bc3b878b04d6ebe82fdb
chenjizhou <694859042@qq.com>
2019-08-16 17:37:18 +0800

    Initial commit

---
+++ src/main/java/com/www/soa/shiro/entity/User.java
@@ -0,0 +1,13 @@
+package com.www.soa.shiro.entity;
+
+/**
+ * @author 陈急舟
+ * @description
+ * @ClassName: User
+ * @date 2019/8/16 17:33
+ */
+public class User {
+    private String username;
+
+    private String password;
+}
